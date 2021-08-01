8708634c17bc6884f132da43cc95222a6b715a0e
Bryant Chen <kobe2103@gmail.com>
2021-08-01 23:49:38 +0800

    advance_3

--- list_2.py
+++ list_2.py
@@ -5,7 +5,13 @@ while True:
         break # 終止迴圈
     price = input('請輸入價格: ')
     products.append([name, price])
-print(products[0][0]) # 存取二維清單
+
+for p in products:
+    print(p[0], '的價格是', p[1]) # for loop 存取二維清單
+
+
+
+
 
         
         
