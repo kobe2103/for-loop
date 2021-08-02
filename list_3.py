62634ef11978e8cf8d5ecb4560b53e98cfbd4da9
Bryant Chen <kobe2103@gmail.com>
2021-08-02 23:24:04 +0800

    advanced_6

--- list_3.py
+++ list_3.py
@@ -1,12 +1,18 @@
+import os # operating system
+
 # 讀取檔案
 products = [] # 二維清單的大清單
-with open ('products.csv', 'r', encoding='utf-8') as f: # 欲讀取的檔案+編碼
-    for line in f: # for loop
-        if '商品,價格' in line: #如果商品,價格在檔案中的話
-            contiune # 跳過這一迴，執行下一迴，continue只能在迴圈使用
-        name, price = line.strip().split(',') # 等號前面小清單；strip取消分行；split遇逗號切割
-        products.append([name, price]) # 把小清單加到大清單中
-print(products)
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+    print('找到檔案')
+    with open ('products.csv', 'r', encoding='utf-8') as f: # 欲讀取的檔案+編碼
+        for line in f: # for loop
+            if '商品,價格' in line: #如果商品,價格在檔案中的話
+                continue # 跳過這一迴，執行下一迴，continue只能在迴圈使用
+            name, price = line.strip().split(',') # 等號前面小清單；strip取消分行；split遇逗號切割
+            products.append([name, price]) # 把小清單加到大清單中
+    print(products)
+else:
+    print('找不到檔案')
     
 # 讓使用者輸入    
 while True:
